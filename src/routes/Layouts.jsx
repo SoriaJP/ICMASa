1c7b893e5b113b64c77fe0d20c99a0e823d69ac3
Luis-AP <lap18958@gmail.com>
2024-07-11 21:54:19 -0300

    Recomendación de uso de Layouts

--- src/routes/Layouts.jsx
+++ src/routes/Layouts.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import Header from '../components/hedernavfooter/Header.jsx'
-import NavBar from '../components/hedernavfooter/NavBar.jsx';
-import FooterII from '../components/hedernavfooter/FooterII.jsx';
+import Header from "../components/hedernavfooter/Header.jsx";
+import NavBar from "../components/hedernavfooter/NavBar.jsx";
+import FooterII from "../components/hedernavfooter/FooterII.jsx";
+import { Outlet } from "react-router-dom";
 
 const Layout = ({ children }) => {
     return (
-      <>
-        <NavBar />
-        <Header />
-        {children}
-        <FooterII />
-      </>
+        <>
+            <NavBar />
+            <Header />
+            <Outlet />
+            <FooterII />
+        </>
     );
-  };
-  
+};
+
 export default Layout;
--- src/routes/routers.jsx
+++ src/routes/routers.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import { createBrowserRouter} from 'react-router-dom';
-import Layout from './Layouts.jsx';
-import PaginaInicio from '../components/PaginaInicio.jsx';
-import PaginaInstitucion from '../components/PaginaInstitucion.jsx';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Layouts.jsx";
+import PaginaInicio from "../components/PaginaInicio.jsx";
+import PaginaInstitucion from "../components/PaginaInstitucion.jsx";
 
 const routers = createBrowserRouter([
     {
-      path: "/",
-      element: (
-        <Layout>
-            <PaginaInicio />
-        </Layout>
-      ),
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <PaginaInicio />,
+            },
+            {
+                path: "institucion",
+                element: <PaginaInstitucion />,
+            },
+        ],
     },
-    {
-      path: "institucion",
-      element: (
-        <>
-          <Layout>
-            <PaginaInstitucion />
-          </Layout>
-        </>
-      ),
-    },
-  ]);
+]);
 
-  export default routers;
+export default routers;
